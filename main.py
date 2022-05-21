d120dda6636ca08f50a9957076845b94d51f16bd
freexrunner <freexrunner@yandex.ru>
2022-05-21 23:12:25 +0300

    start

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ Created on Sat May 21 22:55:09 2022
 """
 
 import sys
+from PyQt5 import uic
+from vendor_id import *
 
+
+class FormatVendorID(QtWidgets.QMainWidow) :
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    myapp = CableCalcMain()
+    myapp = FormatVendorID()
     myapp.show()
     sys.exit(app.exec_())
